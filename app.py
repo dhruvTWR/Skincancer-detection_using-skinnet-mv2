90a177b1c98ced69eebd9279a137fc640af0285d
Pratik birla <pratikkbirla@gmail.com>
2025-04-08 22:26:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     label, prob = predict(image)
     st.markdown(f"### 🔍 Prediction: **{label}**")
